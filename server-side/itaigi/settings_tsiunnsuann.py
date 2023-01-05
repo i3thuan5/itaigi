dfa21a5b69be667db98690e0930644594d932c6f
Bauzak <hong@ithuan.tw>
2023-01-05 14:36:24 +0800

    SECRET_KEY khioh去 .env。

---
+++ server-side/itaigi/settings_tsiunnsuann.py
@@ -0,0 +1,26 @@
+import os
+from .settings import * # noqa
+
+VIRTUAL_HOST = os.getenv('VIRTUAL_HOST').split(',')
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+DEBUG = False
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+LANGUAGE_COOKIE_SECURE = True
+
+CSRF_COOKIE_SAMESITE = 'Strict'
+SESSION_COOKIE_SAMESITE = 'Strict'
+
+CSRF_COOKIE_HTTPONLY = True
+SESSION_COOKIE_HTTPONLY = True
+
+if os.getenv('SSL_REDIRECT', default=True) and VIRTUAL_HOST != ['localhost']:
+    # Tsi̍t-pîng要求HTTPS，因為localhost開發時袂按呢做，所以開發時關--起-來。
+    SECURE_SSL_REDIRECT = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+    SECURE_HSTS_SECONDS = 10
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
