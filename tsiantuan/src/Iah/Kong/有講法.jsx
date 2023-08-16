567bce9993b1070b35f6137bad294d91a92d96f9
Tira <hong@ithuan.tw>
2023-08-16 10:20:54 +0800

    加tshiau下底的廣告。

--- tsiantuan/src/Iah/Kong/有講法.jsx
+++ tsiantuan/src/Iah/Kong/有講法.jsx
@@ -9,12 +9,31 @@ import Debug from 'debug';
 var debug = Debug('itaigi:Kong有講法');
 
 export default class 有講法 extends React.Component {
+  tsusun_kaisiau() {
+    window.open(
+      'https://docs.google.com/forms/d/e/1FAIpQLSc4Q5HpkjtcN6hmig6nnwu6YuSwnL8Gy6yS7dImBs2kbS0Hsw/viewform'
+    );
+  }
   render() {
     return (
     <div className='kongHuat'>
-      <div className='分享'>
-        <span>哇！原來「{ this.props.華語關鍵字 }」是這樣子講的！快分享給你的朋友知道吧</span>
-        <分享鍵 pathname={this.props.pathname}/>
+      <div className='tsusun-kaisiau tsusunkaisiau'>
+        <span>有任何對於台語的疑難雜症嗎？iTaigi提供1對1台語諮詢服務，30分鐘讓你問到飽！</span>
+
+          <div
+            className="ui button labeled"
+          >
+            <a className="ui basic label">
+              NT$1,000 / 30分鐘
+            </a>
+            <div
+              className="ui primary button"
+              onClick={this.tsusun_kaisiau.bind(this)}
+            >
+              <i className="heart icon"></i>
+              立即預約諮詢
+            </div>
+        </div>
       </div>
       {this.props.內容.列表.map((g) => (
         <GuaGi id={g.外語項目編號}
