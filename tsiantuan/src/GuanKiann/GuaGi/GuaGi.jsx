03bb1b6f664f4c8fd43409a5441d0bcb878c12c8
Tira <hong@ithuan.tw>
2023-08-16 11:08:47 +0800

    調版面

--- tsiantuan/src/GuanKiann/GuaGi/GuaGi.jsx
+++ tsiantuan/src/GuanKiann/GuaGi/GuaGi.jsx
@@ -75,22 +75,33 @@ export default class GuaGi extends React.Component {
                />
             </div>
           </div>
+
           <div className='ui su card'>
             <div className='content'>
               <h3 className='ui header'>
                 <i className='cloud upload icon'></i>
-                更多講法：
+                知道更多講法
               </h3>
               如果還想知道「{this.props.華語關鍵字}」的其他講法，你可以：
               <h4>註冊並公開發問</h4>
+              <p>
+                <i className="comment icon"></i>為什麼要註冊？<br/>
+                為了讓iTaigi不被廣告、機器人打擾，需要大家註冊再使用。
+              </p>
               <button
-                className="ui primary button"
+                className="ui green button"
                 // onClick={this.sprint_googleform.bind(this)}
               >
                 註冊並公開發問
               </button>
-              <h4>1對1台語諮詢</h4>
 
+              <h3 className='ui header'>
+                <i className='cloud upload icon'></i>
+                1對1台語諮詢
+              </h3>
+              <p>
+                如果還想知道「{this.props.華語關鍵字}」的講法或其他補充資料，iTaigi提供1對1台語諮詢服務，可以問像是工作上的應答、生活情況、文字翻譯……，30分鐘不限主題讓你問到飽！
+              </p>
               <div
                 className="ui button labeled"
               >
@@ -98,14 +109,16 @@ export default class GuaGi extends React.Component {
                   NT$1,000 / 30分鐘
                 </a>
                 <div
-                  className="ui primary button"
+                  className="ui green button"
                   onClick={this.tsusun_kaisiau.bind(this)}
                 >
                   立即預約諮詢
                 </div>
-            </div>
+              </div>
             </div>
           </div>
+
+
         </div>
       </div>
     </div>
