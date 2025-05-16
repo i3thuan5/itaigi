d4309985960dccd26f1f6c214b59f2971d5c9003
Tia i X570-ACE <hong@ithuan.tw>
2025-05-16 20:06:05 +0800

    設定behave用sqlite

---
+++ server-side/itaigi/settings_behave.py
@@ -0,0 +1,14 @@
+from .settings import INSTALLED_APPS
+from .settings import *  # noqa
+
+INSTALLED_APPS += [
+    'behave_django',
+]
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
